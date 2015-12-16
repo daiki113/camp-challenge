1c2696bfd4984a6d9684a3271f8f9a0e5045111c
daiki sawahata <sawahata0113@gmail.com>
2015-12-16 17:38:16 +0900

    no message

---
+++ Challenge PHP 5/challenge5_1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Challenge1１</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+
+<p>challenge１</p>
+<form action="kadai2.php" method="POST" accept-charset="utf-8">
+<p>
+名前：
+<input type="text" name="name" value="" placeholder="">
+</p>
+
+<p>
+	性別：
+	<input type="radio" name="seibetu" value="男" placeholder="">
+	男
+	<input type="radio" name="seibetu" value="女" placeholder="">
+	女</p>
+
+<p>
+	趣味：
+	<textarea name="hobby"></textarea>
+</p>
+	<input type="submit" name="" value="送信">
+</form>
+
---
+++ Challenge PHP 5/challenge5_5.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+ </head>
+ <body>
+ 	<form enctype="multipart/form-data"
+ 	action="sample.php"method="post">
+ 	File Upload:<input name= "userfile"type="file/">
+ 	</form>
+ </body>
+ </html>
